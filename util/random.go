910a1504dd61dbd4d5969aba21d05b675a131cbe
César Almeida <cesar.almeida@vocedm.com.br>
2023-02-02 07:11:19 -0300

    fixed Random String Generator

--- util/random.go
+++ util/random.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+	"time"
 )
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func RandomString(number int) string {
 	var sb strings.Builder
