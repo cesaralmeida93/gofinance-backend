4f9eb2d080e019358608039fbd860af20866e984
César Almeida <cesar.almeida@vocedm.com.br>
2023-10-23 01:18:51 -0300

    created util jwt.go

---
+++ util/auth.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func ValidateToken(ctx *gin.Context, token string) error {
+	claims := &Claims{}
+	var jwtSignedKey = []byte("secret_key")
+	tokenParse, err := jwt.ParseWithClaims(token, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtSignedKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			ctx.JSON(http.StatusUnauthorized, err)
+			return err
+		}
+		ctx.JSON(http.StatusBadRequest, err)
+		return err
+	}
+
+	if !tokenParse.Valid {
+		ctx.JSON(http.StatusUnauthorized, "Token is invalid")
+		return nil
+	}
+
+	ctx.Next()
+	return nil
+}
